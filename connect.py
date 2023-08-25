40d15e333cd9c3feae982676586bf2092e0c0493
AndriwRC <randriw4@gmail.com>
2023-08-24 22:25:26 -0500

    inserta 5 registros en la base de datos

--- connect.py
+++ connect.py
@@ -2,8 +2,19 @@ import sys
 import pyodbc
 conn= pyodbc.connect(r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\cuc\Documents\RegistroNotas\bdprueba.accdb;")
 cursor=conn.cursor()
-cursor.execute("SELECT * FROM persona1")
-for row in cursor.fetchall():
-    print(row)
+
+datos_persona = [
+    {"Nombre": "Alice", "Apellido": "Johnson", "Edad": 25},
+    {"Nombre": "Bob", "Apellido": "Smith", "Edad": 32},
+    {"Nombre": "Charlie", "Apellido": "Brown", "Edad": 28},
+    {"Nombre": "David", "Apellido": "Davis", "Edad": 40},
+    {"Nombre": "Eve", "Apellido": "Taylor", "Edad": 22}
+]
+
+for persona in datos_persona:
+    query = "INSERT INTO persona1 (Nombre, Apellido, Edad) VALUES (?, ?, ?)"
+    cursor.execute(query, (persona["Nombre"], persona["Apellido"], persona["Edad"]))
+    conn.commit()
+
 cursor.close
 conn.close
